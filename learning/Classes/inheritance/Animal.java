1abaaeb013e476dd481921ce20dffc1be4bf9474
Utkarsh Pandey <utk2665@gmail.com>
2022-07-17 21:50:38 +0530

    Inheritance

--- learning/Classes/inheritance/Animal.java
+++ learning/Classes/inheritance/Animal.java
@@ -1,16 +1,25 @@
 package Classes.inheritance;
 
 public class Animal {
+    private String name;
     private int brain;
     private int body;
     private int size;
     private int weight;
     
-    public Animal(int brain, int body, int size, int weight) {
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
         this.brain = brain;
         this.body = body;
         this.size = size;
         this.weight = weight;
+    }
+
+    public void eat(){
+
+    }
+    public void move(){
+
     }
 
     public int getBrain() {
---
+++ learning/Classes/inheritance/Main.java
@@ -0,0 +1,8 @@
+package Classes.inheritance;
+
+public class Main {
+    Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+    Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "Long Silky");
+    dog.eat();
+}
